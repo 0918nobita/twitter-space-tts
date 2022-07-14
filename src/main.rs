7490bcde445f7faa55953e0082650e1865bbcc29
0918nobita <nobita.0918@gmail.com>
2022-07-14 11:24:51 +0900

    VOICEVOX による音声合成の結果をファイルに保存する部分を実装した

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
+use std::io::Write;
+
 #[tokio::main]
-async fn main() -> reqwest::Result<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
-    let res = client
+
+    let synthesis_query = client
         .post("http://localhost:50021/audio_query")
         .query(&[("text", "こんにちは"), ("speaker", "0")])
         .send()
         .await?
         .json::<serde_json::Value>()
         .await?;
-    println!("{:?}", res);
+
+    let wav_bytes = client
+        .post("http://localhost:50021/synthesis")
+        .query(&[("speaker", "0")])
+        .json(&synthesis_query)
+        .send()
+        .await?
+        .bytes()
+        .await?;
+
+    std::fs::File::create("output.wav")?.write_all(&wav_bytes)?;
+
     Ok(())
 }
